2bc458a68baefca035104bf00b1e4aa7d38f9717
Coloneljm <114030655+Coloneljm@users.noreply.github.com>
2022-09-21 22:00:37 +0100

    Add files via upload

--- EXO1/src/Main.java
+++ EXO1/src/Main.java
@@ -1,42 +1,62 @@
 import java.util.Scanner;
-
+import java.lang.String;
 public class Main {
+
     public static void main(String[] args) {
-        Scanner scan= new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("Initialisation by Junior Medjo ");
         String chaine;
         String mot;
-        int occurence=0;
-        boolean trouve;
+        int occurence = 0;
+        boolean trouve = false;
+
         System.out.println("entrez une chaine de caractere");
-        chaine= scan.nextLine();
-        System.out.println("la chaine entree est "+ chaine+ " et sa taille est "+chaine.length());
+        chaine = scan.nextLine();
+        System.out.println("la chaine entree est " + chaine + " et sa taille est " + chaine.length());
         System.out.println(" ");
         System.out.println("Veuillez entrer un mot et nous allons verifier qu'il existe dans la chaine");
         mot = scan.nextLine();
-        int i=0;
-        while ( i<chaine.length()-1){
-            trouve= false;
-            if ((chaine.charAt(i) == mot.charAt(0))){
-                int j;
-                for (j=0;j<mot.length();j++){
-                    if (chaine.charAt(i)==mot.charAt(j)){
-                        trouve=true;
-                    }
-                    else {
-                        trouve = false;
-                    }
-                    i++;
 
+
+
+
+        int x = chaine.length();
+        int y = mot.length();
+        char[] tab = new char[x];
+        char[] tab2 = new char[y];
+        int i;
+        for (i = 0; i < x; i++)
+            tab[i] = chaine.charAt(i);
+        for (i = 0; i <y-1; i++)
+            tab2[i] = mot.charAt(i);
+
+
+        for ( i=0;i<=x-1;i++){
+            int j=0, k=i, count=0;
+            if (tab[i]==tab2[0]) {
+                while (tab[k] == tab2[j] & j <= y - 1) {
+                    k++;
+                    j++;
+                    count++;
                 }
-                occurence+=1;
+                if (count == y-1) {
+                    occurence += 1;
+                }
             }
         }
-        System.out.println("Dans la chaine "+chaine+" on retrouvre le mot "+mot+" "+ occurence+" fois");
-
-
-
-
-
+        System.out.println("Dans la chaine " + chaine + " on retrouvre le mot " + mot + " " + occurence + " fois");
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
